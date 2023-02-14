37df77747446e6e449e5222f7d9e803857b43ef6
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2023-02-14 16:15:06 +0300

    refactor: Added Plugin Type.

--- src/plugin/plugin.module.ts
+++ src/plugin/plugin.module.ts
@@ -3,6 +3,7 @@ import * as Plugins from "./plugins";
 import { DynamicModule, Module, Provider, forwardRef } from "@nestjs/common";
 
 import { AppModule } from "../app.module";
+import { Plugin } from "./interfaces/plugin.interface";
 import { PluginService } from "./plugin.service";
 
 @Module({
@@ -10,7 +11,7 @@ import { PluginService } from "./plugin.service";
 })
 export class PluginModule {
   public static async registerAsync(): Promise<DynamicModule> {
-    const pluginTypes: Provider<any>[] = Object.values(Plugins);
+    const pluginTypes: Provider<Plugin>[] = Object.values(Plugins);
     return {
       module: PluginModule,
       providers: [
