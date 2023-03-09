ddd7b8a89cbfab029aa8ff02dabde150fc906144
Yahya Mehmet SAGDUR <ymsagdur@gmail.com>
2023-03-09 10:34:58 +0300

    fix(SHBD-309): fix token module providers

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -3,7 +3,10 @@ import { UserService } from "src/user/user.service";
 import { TokenService } from "./token.service";
 
 @Module({
-  providers: [{ provide: "TokenService", useClass: TokenService }, UserService],
+  providers: [
+    { provide: "TokenService", useClass: TokenService },
+    { provide: "UserService", useClass: UserService },
+  ],
   exports: ["TokenService"],
 })
 export class TokenModule {}
