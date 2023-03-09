01ea49e15c3f5071d70799f35f2f1d7df78737bf
Yahya Mehmet SAGDUR <ymsagdur@gmail.com>
2023-03-09 14:35:06 +0300

    fix(SHBD-315): fix auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post } from "@nestjs/common";
 import { UserCredentials } from "./auth.types";
 
-@Controller("auth")
+@Controller()
 export class AuthController {
   @Post("login")
   async login(userCredentials: UserCredentials) {}
