42b7ad3d7457f27b3d32405b5bea11572b5f85fa
Yahya Mehmet SAGDUR <ymsagdur@gmail.com>
2023-03-09 09:56:38 +0300

    fix(SHBD-309): remove token and user service in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { AppService } from "./app.service";
 import { Module } from "@nestjs/common";
 import { PluginModule } from "./plugin/plugin.module";
 import { PluginTestModule } from "./plugin-test/plugin-test.module";
-import { TokenService } from "./token/token.service";
 import { TokenModule } from "./token/token.module";
 import { AuthModule } from "./auth/auth.module";
-import { UserService } from "./user/user.service";
 
 @Module({
   imports: [
@@ -15,7 +13,7 @@ import { UserService } from "./user/user.service";
     TokenModule,
     AuthModule,
   ],
-  providers: [AppService, TokenService, UserService],
+  providers: [AppService],
   exports: [AppService],
   controllers: [AppController],
 })
