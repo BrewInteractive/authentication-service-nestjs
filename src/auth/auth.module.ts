0d38948a0442b52fbd74cc4f6555f8b3bfe9f1ce
Yahya Mehmet SAGDUR <ymsagdur@gmail.com>
2023-03-09 09:59:56 +0300

    feat(SHBD-309): add user service in auth module providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { TokenModule } from "../token/token.module";
+import { UserService } from "src/user/user.service";
 
 @Module({
   imports: [TokenModule],
-  providers: [AuthService],
+  providers: [AuthService, UserService],
   controllers: [AuthController],
 })
 export class AuthModule {}
