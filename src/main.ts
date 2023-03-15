236c34877969dc41771033205ce54384009df8f8
Yahya Mehmet SAGDUR <ymsagdur@gmail.com>
2023-03-15 23:20:35 +0300

    feat(SHBD-365): Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Authentication Service')
+  .setDescription('Authentication Service is a Nest.js based rest api designed to provide authentication operations by Brew Interactive.')
+  .setVersion('1.0')
+  .addTag('authentication', 'User authentication operations')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
