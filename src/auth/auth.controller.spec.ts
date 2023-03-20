08d5218f30cf07b16d243c12115490538a8bca39
Yahya Mehmet SAGDUR <ymsagdur@gmail.com>
2023-03-20 10:03:16 +0300

    fix: fix for auth controller creating module tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 
 import { AuthController } from "./auth.controller";
 import { UserService } from "../user/user.service";
+import { TokenModule } from "../token/token.module";
 
 describe("AuthController", () => {
   let controller: AuthController;
@@ -9,12 +10,13 @@ describe("AuthController", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TokenModule],
+      providers: [{ provide: "UserService", useClass: UserService }],
       controllers: [AuthController],
-      providers: [UserService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
-    userService = module.get<UserService>(UserService);
+    userService = module.get<UserService>('UserService');
   });
 
   it("should be defined", () => {
