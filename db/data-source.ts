4c805d4bab7ba5bdd2cfa028e8ee96a81222c46a
Yahya Mehmet SAGDUR <ymsagdur@gmail.com>
2023-04-05 10:28:07 +0300

    ci: fix for sonar env

--- db/data-source.ts
+++ db/data-source.ts
@@ -5,7 +5,7 @@ const DB_DRIVE = process.env.DB_DRIVE || "postgres";
 
 export const dataSourceOptions: DataSourceOptions = {
   type: DB_DRIVE as "mysql" | "postgres",
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
