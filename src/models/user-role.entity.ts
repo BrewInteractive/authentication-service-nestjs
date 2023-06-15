29435338a17ff4aa746ef88fe049425280e328c1
ismet kizgin <info@ismetkizgin.com.tr>
2023-06-15 08:29:15 +0300

    fix: fix for users roles model

--- src/models/user-role.entity.ts
+++ src/models/user-role.entity.ts
@@ -3,23 +3,30 @@ import { User } from "./user.entity";
 import {
   Entity,
   PrimaryGeneratedColumn,
-  OneToOne,
   JoinColumn,
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
+  Unique,
 } from "typeorm";
 
 @Entity({ name: "users_roles" })
+@Unique(["user", "role"])
 export class UserRole {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne((type: User) => User, (user) => user.id, { nullable: false })
+  @ManyToOne((type: User) => User, (user) => user.id, {
+    nullable: false,
+    createForeignKeyConstraints: false,
+  })
   @JoinColumn({ name: "user_id" })
   user: User;
 
-  @ManyToOne((type: Role) => Role, (role) => role.name, { nullable: false })
+  @ManyToOne((type: Role) => Role, (role) => role.name, {
+    nullable: false,
+    createForeignKeyConstraints: false,
+  })
   @JoinColumn({ name: "role" })
   role: Role;
 
