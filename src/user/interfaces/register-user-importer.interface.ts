f4efab97ecd8943c7bafde9b9357912f8e9a4034
ismet kizgin <info@ismetkizgin.com.tr>
2023-09-28 15:45:56 +0300

    refactor(TSV-30): fix for register user importer interface

--- src/user/interfaces/register-user-importer.interface.ts
+++ src/user/interfaces/register-user-importer.interface.ts
@@ -1,5 +1,5 @@
 import { User } from "../../models/user.entity";
 
 export interface IRegisterUserImporter {
-  createUserAsync(user: User, appData: object): Promise<Boolean>;
+  createUserAsync(user: User, appData: object): Promise<boolean>;
 }
