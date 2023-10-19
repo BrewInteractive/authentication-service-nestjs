014dd968a964410aa700b923859d10fbbf17c017
Ali Torun <toruna@gmail.com>
2023-10-20 00:00:12 +0300

    test: add email config test

---
+++ src/email/aws-email.config.spec.ts
@@ -0,0 +1,26 @@
+import AwsEmailConfig from "./aws-email.config";
+import config from "../utils/config";
+
+jest.mock("../utils/config", () => ({
+  __esModule: true,
+  default: jest.fn(() => ({
+    awsSesRegion: "fake-region",
+    awsSesAccessKey: "fake-ses-access-key",
+    awsSesSecretKey: "fake-ses-secret-key",
+  })),
+}));
+
+describe("AwsEmailConfig", () => {
+  it("should have the correct configuration properties", () => {
+    const awsEmailConfig = AwsEmailConfig;
+    console.log(awsEmailConfig, config());
+
+    expect(awsEmailConfig.region).toEqual(config().awsSesRegion);
+    expect(awsEmailConfig.accessKeyId).toEqual(config().awsSesAccessKey);
+    expect(awsEmailConfig.secretAccessKey).toEqual(config().awsSesSecretKey);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+});
