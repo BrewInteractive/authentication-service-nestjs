6b713cc8df786935069eba37a60ed17060e80b6a
Ali Torun <toruna@gmail.com>
2023-10-26 16:32:08 +0300

    fix: fix email module

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,35 +1,37 @@
-import { Module, ServiceUnavailableException } from "@nestjs/common";
+import { DynamicModule, Module } from "@nestjs/common";
 import { AutomapperModule } from "@automapper/nestjs";
 import { classes } from "@automapper/classes";
-import { Mapper } from "@automapper/core";
 import config from "../utils/config";
 import { EmailServiceType } from "./enum/email.service.type.enum";
-import { AwsEmailService } from "./aws-email.service";
-import AwsEmailConfig from "./aws-email.config";
+import { AwsEmailService } from "./providers/aws-email.service";
 import { EmailService } from "./email.service";
+import { EmailProfile } from "../auth/mapping-profiles/email.mapping.profile";
+import { AwsEmailConfig } from "./providers/aws-email.config";
+import { ConfigService } from "@nestjs/config";
+import { Mapper } from "@automapper/core";
 
 @Module({
-  imports: [
-    AutomapperModule.forRoot({
-      strategyInitializer: classes(),
-    }),
-  ],
+  imports: [],
   providers: [
     {
-      provide: "IAwsEmailConfig",
-      useValue: AwsEmailConfig,
+      provide: "AwsEmailConfig",
+      useValue: {
+        region: config().awsSesRegion,
+        accessKeyId: config().awsSesAccessKey,
+        secretAccessKey: config().awsSesSecretKey,
+      } as AwsEmailConfig,
     },
+    AwsEmailService,
+    EmailProfile,
     {
-      provide: EmailService,
-      useFactory: (mapper: Mapper) => {
-        const emailServiceType = config().emailService as EmailServiceType;
-        if (emailServiceType == EmailServiceType.AWS) {
-          return new AwsEmailService(mapper, AwsEmailConfig);
-        } else {
-          throw new ServiceUnavailableException(emailServiceType);
-        }
+      provide: "EmailService",
+      useFactory: (awsEmailService: AwsEmailService) => {
+        if (config().emailService === "aws") return awsEmailService;
+        else throw new Error("Invalid email service type");
       },
+      inject: [AwsEmailService],
     },
   ],
+  exports: ["EmailService"],
 })
 export class EmailModule {}
