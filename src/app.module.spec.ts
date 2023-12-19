dec9693b159eeda316cb7c40f9b6f1b746df50ed
ismet kizgin <info@ismetkizgin.com.tr>
2023-12-19 20:57:03 +0300

    test: fix for app module test

---
+++ src/app.module.spec.ts
@@ -0,0 +1,23 @@
+import { AppModule } from "./app.module";
+import { DataSource } from "typeorm";
+import { Test } from "@nestjs/testing";
+import { setupTestDataSourceAsync } from "../test/test-db";
+
+describe("AppModule", () => {
+  let appModule: AppModule;
+
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(DataSource)
+      .useValue(await setupTestDataSourceAsync())
+      .compile();
+
+    appModule = app.get<AppModule>(AppModule);
+  });
+
+  it("Should be defined", () => {
+    expect(appModule).toBeDefined();
+  });
+});
