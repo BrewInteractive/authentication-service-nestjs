156435f2e04400c4ae773adcce10d7716ed7e29e
ismet kizgin <info@ismetkizgin.com.tr>
2024-01-29 14:23:04 +0300

    doc: fix swagger config for refresh token controller

--- src/refresh-token/refresh-token.controller.ts
+++ src/refresh-token/refresh-token.controller.ts
@@ -2,8 +2,11 @@ import { Controller, Post, Body, Inject } from "@nestjs/common";
 import { TokenService } from "../token/token.service";
 import { RefreshTokenRequest } from "./dto/refresh-token-request.dto";
 import { RefreshTokenResponse } from "./dto/refresh-token-response.dto";
+import { ApiSecurity, ApiTags } from "@nestjs/swagger";
 
 @Controller("refresh-token")
+@ApiTags("authentication")
+@ApiSecurity("ApiKey")
 export class RefreshTokenController {
   constructor(
     @Inject("TokenService") private readonly tokenService: TokenService
