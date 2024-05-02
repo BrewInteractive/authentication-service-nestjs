1cef6699ace75d70d7cc6872d72fa8c65acee4e5
ismet kizgin <info@ismetkizgin.com.tr>
2024-05-02 09:21:29 +0300

    refactor: fix for using the automapper package

--- src/reset-password/reset-password.module.ts
+++ src/reset-password/reset-password.module.ts
@@ -1,20 +1,11 @@
-import { AutomapperModule } from "@automapper/nestjs";
 import { EmailModule } from "../email/email.module";
 import { Module } from "@nestjs/common";
 import { ResetPasswordController } from "./reset-password.controller";
 import { TemplateModule } from "../template/template.module";
 import { UserModule } from "../user/user.module";
-import { classes } from "@automapper/classes";
 
 @Module({
-  imports: [
-    UserModule,
-    EmailModule,
-    TemplateModule,
-    AutomapperModule.forRoot({
-      strategyInitializer: classes(),
-    }),
-  ],
+  imports: [UserModule, EmailModule, TemplateModule],
   controllers: [ResetPasswordController],
 })
 export class ResetPasswordModule {}
