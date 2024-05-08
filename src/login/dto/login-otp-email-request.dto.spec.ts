17b886c578249040293a711b45a1e449632467fd
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2024-05-08 11:41:58 +0300

    test: renamed tests.

--- src/login/dto/login-otp-email-request.dto.spec.ts
+++ src/login/dto/login-otp-email-request.dto.spec.ts
@@ -3,7 +3,7 @@ import { faker } from "@faker-js/faker";
 import { validate } from "class-validator";
 
 describe("LoginOtpEmailRequest Validation", () => {
-  it("should successful verification be performed", async () => {
+  it("should be verified", async () => {
     const loginOtpEmailRequest = new LoginOtpEmailRequest();
     loginOtpEmailRequest.email = faker.internet.email();
     loginOtpEmailRequest.otpValue = faker.datatype.string(6);
@@ -12,7 +12,7 @@ describe("LoginOtpEmailRequest Validation", () => {
     expect(errors.length).toBe(0);
   });
 
-  it("should unsuccessful verification be performed", async () => {
+  it("shouldn't be verified", async () => {
     const loginOtpEmailRequest = new LoginOtpEmailRequest();
     loginOtpEmailRequest.email = faker.lorem.words(1);
     loginOtpEmailRequest.otpValue = faker.datatype.string(6);
