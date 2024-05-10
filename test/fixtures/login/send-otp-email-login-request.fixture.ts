a85fe41528c0226d73513a3bbd13e3c7112f7a08
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2024-05-10 14:39:37 +0300

    style: code cleaned up.

--- test/fixtures/login/send-otp-email-login-request.fixture.ts
+++ test/fixtures/login/send-otp-email-login-request.fixture.ts
@@ -1,7 +1,7 @@
-import { SendLoginOtpEmailRequest } from "../../../src/login/dto/send-login-otp-email-request.dto";
 import { Mock } from "mockingbird";
+import { SendLoginOtpEmailRequest } from "../../../src/login/dto/send-login-otp-email-request.dto";
 
-export class SendLoginOtpEmailRequestFixture extends SendLoginOtpEmailRequest{
-    @Mock((faker) => faker.internet.email())
-    email: string;
+export class SendLoginOtpEmailRequestFixture extends SendLoginOtpEmailRequest {
+  @Mock((faker) => faker.internet.email())
+  email: string;
 }
