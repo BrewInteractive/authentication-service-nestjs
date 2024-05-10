9647f82a5150bd5c41f601cf30e2705d004a8799
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2024-05-10 14:40:56 +0300

    style: removed unused.

--- test/integration/send-otp-email-login.e2e-spec.ts
+++ test/integration/send-otp-email-login.e2e-spec.ts
@@ -2,16 +2,16 @@ import * as request from "supertest";
 
 import { DataSource, Repository } from "typeorm";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
-import { LoginRequestFixture, OtpFixture, UserFixture } from "../fixtures";
+import { OtpFixture, UserFixture } from "../fixtures";
 import { Test, TestingModule } from "@nestjs/testing";
 
 import { AppModule } from "../../src/app.module";
 import { MockFactory } from "mockingbird";
-import { User } from "../../src/entities/user.entity";
-import { setupTestDataSourceAsync } from "../test-db";
 import { Otp } from "../../src/entities";
 import { SendLoginOtpEmailRequestFixture } from "../fixtures/login/send-otp-email-login-request.fixture";
+import { User } from "../../src/entities/user.entity";
 import { faker } from "@faker-js/faker";
+import { setupTestDataSourceAsync } from "../test-db";
 
 describe("SendLoginOtpEmailController (e2e)", () => {
   let app: INestApplication;
@@ -42,17 +42,23 @@ describe("SendLoginOtpEmailController (e2e)", () => {
   describe("POST /send-login-otp-email", () => {
     it("Should return send otp result when active otp exists", async () => {
       let user = MockFactory(UserFixture).one().hashPassword();
-      let otp = MockFactory(OtpFixture).mutate({
-        channel : { email : user.email },
-        expiresAt: faker.date.future(),
-      }).one();
+      let otp = MockFactory(OtpFixture)
+        .mutate({
+          channel: { email: user.email },
+          expiresAt: faker.date.future(),
+        })
+        .one();
 
       await userRepository.save(user);
       await otpRepository.save(otp);
 
-      const sendLoginOtpEmailRequest = MockFactory(SendLoginOtpEmailRequestFixture).mutate({
+      const sendLoginOtpEmailRequest = MockFactory(
+        SendLoginOtpEmailRequestFixture
+      )
+        .mutate({
           email: user.email,
-        }).one();
+        })
+        .one();
 
       const responseEmail = await request(app.getHttpServer())
         .post("/send-login-otp-email")
@@ -60,36 +66,45 @@ describe("SendLoginOtpEmailController (e2e)", () => {
         .expect(201);
 
       expect(responseEmail.body.isSent).toEqual(false);
-      expect(new Date(responseEmail.body.expiresAt)).toEqual(new Date(otp.expiresAt));
+      expect(new Date(responseEmail.body.expiresAt)).toEqual(
+        new Date(otp.expiresAt)
+      );
     });
 
     it("Should return send otp result when no active otp exists", async () => {
-        let user = MockFactory(UserFixture).one().hashPassword();
-        await userRepository.save(user);
+      let user = MockFactory(UserFixture).one().hashPassword();
+      await userRepository.save(user);
 
-        const sendLoginOtpEmailRequest = MockFactory(SendLoginOtpEmailRequestFixture)
-          .mutate({
-            email: user.email,
-          }).one();
+      const sendLoginOtpEmailRequest = MockFactory(
+        SendLoginOtpEmailRequestFixture
+      )
+        .mutate({
+          email: user.email,
+        })
+        .one();
 
-        const responseEmail = await request(app.getHttpServer())
-          .post("/send-login-otp-email")
-          .send(sendLoginOtpEmailRequest)
-          .expect(201);
+      const responseEmail = await request(app.getHttpServer())
+        .post("/send-login-otp-email")
+        .send(sendLoginOtpEmailRequest)
+        .expect(201);
 
-        expect(responseEmail.body.isSent).toEqual(true);
-        expect(new Date(responseEmail.body.expiresAt).getTime()).toBeGreaterThan(new Date().getTime())
+      expect(responseEmail.body.isSent).toEqual(true);
+      expect(new Date(responseEmail.body.expiresAt).getTime()).toBeGreaterThan(
+        new Date().getTime()
+      );
     });
 
     it("Should return unauthorized error when no user with given email exists", async () => {
-        const sendLoginOtpEmailRequest = MockFactory(SendLoginOtpEmailRequestFixture).one();
+      const sendLoginOtpEmailRequest = MockFactory(
+        SendLoginOtpEmailRequestFixture
+      ).one();
 
-        const response = await request(app.getHttpServer())
-          .post("/send-login-otp-email")
-          .send(sendLoginOtpEmailRequest)
-          .expect(401);
+      const response = await request(app.getHttpServer())
+        .post("/send-login-otp-email")
+        .send(sendLoginOtpEmailRequest)
+        .expect(401);
 
-        expect(response.body.message).toEqual("User doesn't exist.");
+      expect(response.body.message).toEqual("User doesn't exist.");
     });
   });
 });
