2b48072deb59200c19429924cbff50d6cdd0366c
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2024-05-13 13:18:15 +0300

    fix: fix for nullable constraint for expires_at column in user-reset-password-request entity

---
+++ db/postgres/migrations/1715595241211-AlterResetPasswordRequestExpiresAtNullableFalse.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterResetPasswordRequestExpiresAtNullableFalse1715595241211 implements MigrationInterface {
+    name = 'AlterResetPasswordRequestExpiresAtNullableFalse1715595241211'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users_reset_password_requests" ALTER COLUMN "expires_at" SET NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users_reset_password_requests" ALTER COLUMN "expires_at" DROP NOT NULL`);
+    }
+
+}
