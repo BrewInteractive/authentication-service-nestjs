e10a8202ccc2acddda84ad45b26ff91a784712e2
Ali Torun <toruna@gmail.com>
2024-05-13 16:31:35 +0300

    refactor: Update reset password module imports and remove unused code

--- src/reset-password/reset-password.module.ts
+++ src/reset-password/reset-password.module.ts
@@ -1,10 +1,8 @@
 import { User, UserResetPasswordRequest } from "../entities";
 
-import { EmailModule } from "../email/email.module";
 import { Module } from "@nestjs/common";
 import { ResetPasswordController } from "./reset-password.controller";
 import { ResetPasswordService } from "./reset-password.service";
-import { TemplateModule } from "../template/template.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
