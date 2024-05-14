fc067a51b881766b0597e019ac0de1e30710b119
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2024-05-14 14:29:24 +0300

    fix: add NOW() update for reset_password request expires_at before setting NOT NULL constraint

--- db/postgres/migrations/1715595241211-AlterResetPasswordRequestExpiresAtNullableFalse.ts
+++ db/postgres/migrations/1715595241211-AlterResetPasswordRequestExpiresAtNullableFalse.ts
@@ -4,6 +4,7 @@ export class AlterResetPasswordRequestExpiresAtNullableFalse1715595241211 implem
     name = 'AlterResetPasswordRequestExpiresAtNullableFalse1715595241211'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`UPDATE "users_reset_password_requests" SET "expires_at" = NOW() WHERE "expires_at" IS NULL;`);
         await queryRunner.query(`ALTER TABLE "users_reset_password_requests" ALTER COLUMN "expires_at" SET NOT NULL`);
     }
 
