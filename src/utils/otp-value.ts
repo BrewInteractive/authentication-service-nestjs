367740410b1cf407bb22dbc6771f789be957ca7d
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2024-05-23 20:34:15 +0300

    chore: fix format for otp value generator

--- src/utils/otp-value.ts
+++ src/utils/otp-value.ts
@@ -1,7 +1,7 @@
 export class OtpValue {
-    static generate(
-        length: number,
-    ): string {
-        return String(Array.from({ length }, () => Math.floor(Math.random() * 10)).join(''));
-    }
+  static generate(length: number): string {
+    return String(
+      Array.from({ length }, () => Math.floor(Math.random() * 10)).join("")
+    );
+  }
 }
