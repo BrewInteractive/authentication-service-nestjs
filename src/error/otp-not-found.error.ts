87125ded23b626d0de600bbba95928036f3f453e
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2024-05-27 10:02:35 +0300

    refactor: refactored error message.

--- src/error/otp-not-found.error.ts
+++ src/error/otp-not-found.error.ts
@@ -5,7 +5,7 @@ import { appConfig } from "../config";
 export class OtpNotFoundError extends ExtendedError<ErrorExtensions> {
   constructor() {
     super();
-    this.message = "Otp not found";
+    this.message = "Otp not found.";
     this.extensions = {
       code: appConfig().errorCodePrefix + "006",
     };
