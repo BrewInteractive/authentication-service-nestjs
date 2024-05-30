13361183fc502befafcb6e748cc1fb86097fd704
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2024-05-30 13:46:47 +0300

    test: removed unused import and variable.

--- src/login/send-login-otp-email.controller.spec.ts
+++ src/login/send-login-otp-email.controller.spec.ts
@@ -15,18 +15,14 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { AutomapperModule } from "@automapper/nestjs";
 import { ConfigModule } from "@nestjs/config";
 import { EventEmitterModule } from "@nestjs/event-emitter";
-import { InvalidCredentialsError } from "../error";
 import { MockFactory } from "mockingbird";
 import { OtpModule } from "../otp/otp.module";
 import { OtpService } from "../otp/otp.service";
 import { SendLoginOtpEmailController } from "./send-login-otp-email.controller";
-import { SendOtpResult } from "../otp/dto";
 import { TokenModule } from "../token/token.module";
-import { UnauthorizedException } from "@nestjs/common";
 import { UserModule } from "../user/user.module";
 import { UserService } from "../user/user.service";
 import { classes } from "@automapper/classes";
-import exp from "constants";
 import { faker } from "@faker-js/faker";
 import { getRepositoryToken } from "@nestjs/typeorm";
 
@@ -167,12 +163,6 @@ describe("SendLoginOtpEmailController", () => {
     const mockSendLoginOtpEmailRequestDto = MockFactory(
       SendLoginOtpEmailRequestFixture
     ).one();
-
-    const mockValidUser = MockFactory(UserFixture)
-      .mutate({
-        email: mockSendLoginOtpEmailRequestDto.email,
-      })
-      .one();
 
     const mockSendOtpResult = MockFactory(SendOtpResultFixture)
       .mutate({
