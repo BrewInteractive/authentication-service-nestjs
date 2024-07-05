fa4517c7398fe2f09d6e3eba6d02ea48acb7eaf0
ismet kizgin <info@ismetkizgin.com.tr>
2024-07-05 14:49:28 +0300

    test: fix for e2e test

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,3 +1,7 @@
 process.env.EMAIL_SERVICE = "aws";
 process.env.PASSWORD_REGEX =
   "(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}";
+process.env.JWT_ALGORITHM = "HS256";
+process.env.JWT_AUDIENCE = "test";
+process.env.JWT_ISSUER = "test";
+process.env.JWT_SECRET = "test";
