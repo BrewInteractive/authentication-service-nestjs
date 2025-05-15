b2f256b1a5bc582f9e7ffadbc033d2bd002c6f96
ismet kizgin <info@ismetkizgin.com.tr>
2025-05-15 10:17:05 +0300

    test: fix for dto test

--- src/sign-up/dto/send-sign-up-otp-email-request.dto.spec.ts
+++ src/sign-up/dto/send-sign-up-otp-email-request.dto.spec.ts
@@ -1,3 +1,4 @@
+import { PhoneRequestDto } from "../../login/dto/phone.dto";
 import { SendSignUpOtpEmailRequest } from "./send-sign-up-otp-email-request.dto";
 import { faker } from "@faker-js/faker";
 import { validate } from "class-validator";
@@ -27,10 +28,10 @@ describe("SendLoginOtpEmailRequest validation", () => {
 
   it("should fail validation when phone is not provided", async () => {
     const request = new SendSignUpOtpEmailRequest();
-    request.phone = {
-      number: faker.phone.number(),
-      countryCode: faker.string.numeric(3),
-    };
+    const phone = new PhoneRequestDto();
+    phone.number = faker.phone.number();
+    phone.countryCode = faker.string.numeric(3);
+    request.phone = phone;
     const errors = await validate(request);
 
     expect(errors.length).toBe(1);
